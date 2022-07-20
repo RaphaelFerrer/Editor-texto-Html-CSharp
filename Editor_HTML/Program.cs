74dcd9e02c929b36023037d60bff2dcd133c041f
p000276 <p000276@DESK060211112>
2022-07-20 14:30:34 -0300

    Adicionar arquivos de projeto.

---
+++ Editor_HTML/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Editor_HTML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+
+            Console.ReadLine();
+        }
+    }
+}
